b0ce3886038292a44dc1a9a40f8918356a2695d5
Fochi1999 <73483730+Fochi1999@users.noreply.github.com>
2023-10-19 14:19:19 +0100

    Initial commit

---
+++ src/main/java/it/unipi/mrcv/Main.java
@@ -0,0 +1,17 @@
+package it.unipi.mrcv;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Invio with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+
+
+        // Press Maiusc+F10 or click the green arrow button in the gutter to run the code.
+        
+    }
+}
