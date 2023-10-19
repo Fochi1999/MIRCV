faf060d68b9bbb270fb463bfb8740f0326a660d5
Daniele Giaquinta <104736631+Gia-qui@users.noreply.github.com>
2023-10-19 16:33:52 +0200

    Initial commit

---
+++ src/main/java/it/unipi/mrcv/DictionaryElem.java
@@ -0,0 +1,76 @@
+package src.main.java.org;
+
+public class DictionaryElem {
+    // term
+    private String term;
+    // document frequency (n. of documents containing the term)
+    private int df;
+    // collection frequency (n. of occurrences)
+    private int cf;
+    // offset of the posting list containing docIds
+    private long offsetDoc;
+    // offset of the posting list containing frequencies
+    private long offsetFreq;
+    // length of the posting list
+    private int length;
+
+    // default constructor
+    public DictionaryElem(){
+    };
+
+    public DictionaryElem(String term){
+        this.term = term;
+        this.df = 0;
+        this.cf = 0;
+        this.offsetDoc = 0;
+        this.offsetFreq = 0;
+        this.length = 0;
+    };
+
+    // set methods
+    public void setDf(int df){
+        this.df = df;
+    };
+
+    public void setCf(int cf){
+        this.cf = cf;
+    };
+
+    public void setOffsetDoc(int offsetDoc){
+        this.offsetDoc = offsetDoc;
+    };
+
+    public void setOffsetFreq(int offsetFreq){
+        this.offsetFreq = offsetFreq;
+    };
+
+    public void setLength(int length){
+        this.length = length;
+    };
+
+    // get methods
+    public String getTerm(){
+        return this.term;
+    };
+
+    public int getDf(){
+        return this.df;
+    };
+
+    public int getCf(){
+        return this.cf;
+    };
+
+    public long getOffsetDoc(){
+        return this.offsetDoc;
+    };
+
+    public long getOffsetFreq(){
+        return this.offsetFreq;
+    };
+
+    public int getLength(){
+        return this.length;
+    };
+
+}
---
+++ src/main/java/it/unipi/mrcv/Posting.java
@@ -0,0 +1,39 @@
+package src.main.java.org;
+
+public class Posting {
+
+    private int docid;
+    private int frequency;
+
+    // default constructor
+    public Posting() {
+    }
+
+    // constructor that takes the docid and frequency as input
+    public Posting(int docid, int frequency) {
+        this.docid = docid;
+        this.frequency = frequency;
+    }
+
+    // utility methods
+    public int getDocid() {
+        return docid;
+    }
+
+    public void setDocid(int docid) {
+        this.docid = docid;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    @Override
+    public String toString() {
+        return "Posting{docid=" + docid + ", frequency=" + frequency + '}';
+    }
+}
