17ee1d599c893f88ea27c9058ac6ccac6095daa9
Daniele Giaquinta <deft56.gg@gmail.com>
2023-11-07 17:37:03 +0100

    wtf

--- src/main/java/it/unipi/mrcv/Main.java
+++ src/main/java/it/unipi/mrcv/Main.java
@@ -1,18 +1,37 @@
 package it.unipi.mrcv;
 
+import it.unipi.mrcv.data_structures.*;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.IntBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import it.unipi.mrcv.index.SPIMI;
+
+import static it.unipi.mrcv.index.SPIMI.dictionary;
+import static it.unipi.mrcv.index.SPIMI.postingLists;
+
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        // Press Alt+Invio with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+        SPIMI.exeSPIMI("sample.txt");
+        SPIMI.readDictionary("voc_0");
 
-        SPIMI.exeSPIMI("collection.tsv");
-        // Press Maiusc+F10 or click the green arrow button in the gutter to run the code.
+
 
     }
 }
+
