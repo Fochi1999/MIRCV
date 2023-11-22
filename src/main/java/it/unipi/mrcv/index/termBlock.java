2301b74e71b28535ab0dda7b9200ccb816483ca0
Fochi1999 <73483730+Fochi1999@users.noreply.github.com>
2023-11-22 12:01:37 +0100

    Changes to dictionaryElem and termBlock

--- src/main/java/it/unipi/mrcv/index/termBlock.java
+++ src/main/java/it/unipi/mrcv/index/termBlock.java
@@ -15,7 +15,7 @@ public class termBlock{
         term.setCf(cf);
         term.setOffsetDoc(offsetDoc);
         term.setOffsetFreq(offsetFreq);
-        term.setLength(length);
+        term.setLengthDoc(length);
         this.numBlock=numBlock;
     }
     public termBlock(){
@@ -37,11 +37,13 @@ public class termBlock{
     }
 
     public void copyBlock(termBlock t){
+        this.term.setTerm(t.getTerm());
         this.term.setDf(t.getDictionaryElem().getDf());
         this.term.setCf(t.getDictionaryElem().getCf());
         this.term.setOffsetDoc(t.getDictionaryElem().getOffsetDoc());
         this.term.setOffsetFreq(t.getDictionaryElem().getOffsetFreq());
-        this.term.setLength(t.getDictionaryElem().getLength());
+        this.term.setLengthDoc(t.getDictionaryElem().getLengthDoc());
+        this.term.setLengthFreq(t.getDictionaryElem().getLengthFreq());
         this.numBlock=t.getNumBlock();
     }
 }
